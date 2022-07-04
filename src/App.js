b0c05e0d3ad3d69f7b8ccd62235300b3942b7e71
LCKuo <asdfg6311@gmail.com>
2022-07-04 14:20:04 +0800

    新增切換場景功能

--- src/App.js
+++ src/App.js
@@ -9,20 +9,41 @@ import { Box } from './components/Box'
 import { Skybox } from './components/Skybox'
 import { Loading } from './components/Loading'
 import { Terrain } from 'three-landscape'
+//https://s.studiobinder.com/wp-content/uploads/2019/11/360-Video-Featured-StudioBinder-Compressed.jpg
+//https://ictjournal.itri.org.tw/WebTools/Thumbnail.ashx?Siteid=654246032665636316&MmmID=654304432061644411&fd=Messagess_Pics&Pname=shutterstock_580893679.jpg&nw=600
 
 export default function App() {
+
+  const [Url, setUrl] = React.useState('/DayInTheClouds4k.png')
+
+  function gotoLobby() {
+    setUrl("/DayInTheClouds4k.png")
+  }
+  function gotoCloud() {
+    setUrl("/sky.jpg")
+  }
+  function gotoTaipower() {
+    setUrl("/tp.jpg")
+  }
+
+
   const root = '/maps/CustomTerrainShape'
   return (
-    <Canvas camera={{ fov: 50, far: 6000 }}>
-      <ambientLight intensity={0.5} />
+    <>
+      <button onClick={gotoLobby}>MetaStar大廳</button>
+      <button onClick={gotoTaipower}>台電大林廠</button>
+      <button onClick={gotoCloud}>天空</button>
 
-      <OrbitControls maxDistance={2000} />
+      <Canvas camera={{ fov: 50, far: 6000 }}>
+        <ambientLight intensity={0.5} />
 
-      <Suspense fallback={<Loading />}>
-        <Skybox fog={false} />
-        <Environment preset="park" rotation={[0, Math.PI / 2, 0]} />
-        <fog attach="fog" args={['#74bbd0', 0, 2000]} />
-      </Suspense>
-    </Canvas>
+        <OrbitControls maxDistance={2000} />
+        <Suspense fallback={<Loading />}>
+          <Skybox fog={false} url={Url} />
+          <Environment preset="park" rotation={[0, Math.PI / 2, 0]} />
+          <fog attach="fog" args={['#74bbd0', 0, 2000]} />
+        </Suspense>
+      </Canvas>
+    </>
   )
 }
--- src/components/Skybox.jsx
+++ src/components/Skybox.jsx
@@ -1,8 +1,9 @@
 import { useLoader } from '@react-three/fiber'
 import { BackSide, TextureLoader } from 'three'
 
-export function Skybox({ fog, path = '/DayInTheClouds4k.png', rotation = [0, Math.PI / 2, 0] }) {
-  const map = useLoader(TextureLoader, path)
+export function Skybox({ url, fog, path = '/DayInTheClouds4k.png', rotation = [0, Math.PI / 2, 0] }) {
+  console.log(url)
+  const map = useLoader(TextureLoader, url)
   return (
     <mesh rotation={rotation}>
       <sphereBufferGeometry args={[2000, 300, 300]} />
